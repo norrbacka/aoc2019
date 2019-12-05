d428f980d08f1d19ca6e361e4e484156092ed9d6
Johannes Norrbacka <johannes.norrbacka@youcal.com>
2019-12-05 16:34:27 +0100

    better part 1

--- Program.fs
+++ Program.fs
@@ -6,5 +6,5 @@ open System
 let main argv =
     //let d11 = Day1.part1()
     //let d12 = Day1.part2()
-    let d21 = Day2.part1()
+    let d21 = Day2.part1_func()
     0 // return an integer exit code
